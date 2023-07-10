b2b9ede705ba3893e2217e54e2da7a884e64833b
lg <lauren@informal.systems>
2023-07-10 18:26:38 +0200

    Add block results grpc endpoint

--- test/e2e/tests/grpc_test.go
+++ test/e2e/tests/grpc_test.go
@@ -5,9 +5,12 @@ import (
 	"testing"
 	"time"
 
+	v1 "github.com/cometbft/cometbft/proto/tendermint/services/block_results/v1"
+
+	"github.com/stretchr/testify/require"
+
 	e2e "github.com/cometbft/cometbft/test/e2e/pkg"
 	"github.com/cometbft/cometbft/version"
-	"github.com/stretchr/testify/require"
 )
 
 func TestGRPC_Version(t *testing.T) {
@@ -28,5 +31,32 @@ func TestGRPC_Version(t *testing.T) {
 		require.Equal(t, version.ABCIVersion, res.ABCI)
 		require.Equal(t, version.P2PProtocol, res.P2P)
 		require.Equal(t, version.BlockProtocol, res.Block)
+	})
+}
+
+func TestGRPC_BlockResults(t *testing.T) {
+	testNode(t, func(t *testing.T, node e2e.Node) {
+		// TODO: What is this?
+		if node.Mode != e2e.ModeFull && node.Mode != e2e.ModeValidator {
+			return
+		}
+
+		nodeClient, err := node.Client()
+		require.NoError(t, err)
+
+		ctx, ctxCancel := context.WithTimeout(context.Background(), time.Minute)
+		defer ctxCancel()
+
+		client, err := node.GRPCClient(ctx)
+		require.NoError(t, err)
+
+		res, err := nodeClient.BlockResults(ctx, nil)
+		require.NoError(t, err)
+		t.Logf("Res: %v", res)
+
+		grpcRes, err := client.GetBlockResults(ctx, v1.GetBlockResultsRequest{})
+		require.NoError(t, err)
+
+		require.Equal(t, res.Height, grpcRes.Height)
 	})
 }

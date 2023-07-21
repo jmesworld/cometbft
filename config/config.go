5c0a81fc28cf90c294af6270e7d13f7100cb5c54
Mikhail Zabaluev <mikhail@informal.systems>
2023-07-21 23:21:21 +0300

    config: disable the pruning service by default

--- config/config.go
+++ config/config.go
@@ -605,7 +605,7 @@ type GRPCPruningServiceConfig struct {
 
 func DefaultGRPCPruningServiceConfig() *GRPCPruningServiceConfig {
 	return &GRPCPruningServiceConfig{
-		Enabled: true,
+		Enabled: false,
 	}
 }
 
